1cf41cf72a4aec70bb8ffe75bda029204b9b32af
RandallWyatt <randallw16@fullsail.edu>
2015-04-18 22:07:32 -0400

    duel week 2

---
+++ Wk2/goal2_duel2/JS/main.js
@@ -0,0 +1,84 @@
+/*
+ Name: Randall Wyatt
+ Date: 04/03/2015
+ AssignmentL Goal1: Assignment: Duel1
+ */
+
+
+//self-executing function
+(function(){
+
+   console.log("FIGHT!!!");
+
+    //player name
+
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round = 0;
+
+    function fight(){
+        //console.log('in the fight function');
+
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+      for (var i= 0; i < 10; i++){
+
+
+
+          var minDamage1 = player1Damage * .5;
+          var minDamage2 = player2Damage * .5;
+          var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+          var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
+
+
+
+           //inflicting damage
+          playerOneHealth-=f1;
+          playerTwoHealth-=f2;
+
+          console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
+
+          var results = winnerCheck();
+            console.log(results);
+
+          if (results === "no w1nner"){
+              round++;
+              alert(playerOneName+":"+playerOneHealth+" *Round "+ round +" OVER*"+ playerTwoName+":"+playerTwoHealth);
+          }else{
+              alert(results);
+              break;
+          }
+
+      }
+};
+    function winnerCheck () {
+    console.log("in winnerCheck FN");
+        var result = "no winner";
+
+        if (playerOneHealth < 1 && playerTwoHealth < 1) {
+            result = "You Both Die";
+        } else if (playerOneHealth < 1) {
+            result = playerTwoName + " WINS!!!"
+        } else if (playerTwoHealth < 1) {
+            result = playerOneName + " WINS!!!"
+
+        };
+
+
+        return result;
+    };
+    //fight starts
+    console.log('program starts');
+    fight();
+
+
+})();
